5fe0d62a14d689522ece487042f555252cb5b410
Andreacetre <paolcemo@gmail.com>
2025-03-25 21:33:23 -0500

    Actualización de conexión con MySQL en la nube

--- src/Config/db.js
+++ src/Config/db.js
@@ -4,8 +4,16 @@ require("dotenv").config();
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   dialect: "mysql",
-  logging: false,
-  pool: { max: 5, min: 0, acquire: 30000, idle: 10000 },
+  port: process.env.DB_PORT || 3306,
+  dialectOptions: {
+    connectTimeout: 60000 // 60 segundos
+  },
+  pool: {
+    max: 10,
+    min: 0,
+    acquire: 60000,
+    idle: 10000
+  }
 });
 
 sequelize
